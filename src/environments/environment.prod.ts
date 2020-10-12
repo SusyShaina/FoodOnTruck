f2999936bffdf544c8d9963b2d501350b9de25f2
Suseela Suresh <susyshaina@gmail.com>
2020-10-12 21:29:26 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDibwb-emfa3U7QoiXV6Td1st_efLK-DyU",
+    authDomain: "foodieshub-789.firebaseapp.com",
+    databaseURL: "https://foodieshub-789.firebaseio.com",
+    projectId: "foodieshub-789",
+    storageBucket: "foodieshub-789.appspot.com",
+    messagingSenderId: "1049271262829"
+  }
 };
